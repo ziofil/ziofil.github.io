0578cfc91b2d95b3e7f91fd3d9ace31f0d0c8942
ziofil <ziofil@users.noreply.github.com>
2023-01-06 09:13:50 -0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+/*
 document.addEventListener("DOMContentLoaded", function() {
     var postHeadings = document.querySelectorAll(".post h2");
 
@@ -6,4 +7,32 @@ document.addEventListener("DOMContentLoaded", function() {
         this.classList.toggle("expanded");
       });
     }
+});
+*/
+// main.js
+window.addEventListener("DOMContentLoaded", function() {
+  var postTitlesContainer = document.getElementById("post-titles");
+
+  for (var i = 1; i <= 2; i++) {
+    // Fetch the content of the post file
+    fetch(`posts/post${i}.html`)
+      .then(function(response) {
+        return response.text();
+      })
+      .then(function(html) {
+        // Extract the title and link from the post file
+        var parser = new DOMParser();
+        var doc = parser.parseFromString(html, "text/html");
+        var title = doc.querySelector("h2").textContent;
+        var link = doc.querySelector("h2 a").getAttribute("href");
+
+        // Insert the title and link into the page
+        var li = document.createElement("li");
+        var a = document.createElement("a");
+        a.textContent = title;
+        a.setAttribute("href", link);
+        li.appendChild(a);
+        postTitlesContainer.appendChild(li);
+      });
+  }
 });
