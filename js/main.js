78f87200307f4c4d6e8b7e1c3a179c70a53441b6
ziofil <ziofil@users.noreply.github.com>
2023-01-06 08:04:36 -0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,10 +1,12 @@
 document.addEventListener("DOMContentLoaded", function() {
-  var postHeadings = document.querySelectorAll(".post h2");
+  console.log("DOMContentLoaded has fired");
+  document.addEventListener("DOMContentLoaded", function() {
+    var postHeadings = document.querySelectorAll(".post h2");
 
-  for (var i = 0; i < postHeadings.length; i++) {
-    postHeadings[i].addEventListener("click", function() {
-      this.classList.toggle("expanded");
-    });
-  }
+    for (var i = 0; i < postHeadings.length; i++) {
+      postHeadings[i].addEventListener("click", function() {
+        this.classList.toggle("expanded");
+      });
+    }
+  });
 });
-
