8714fa92abafa56b9d3516f1d2752817e0667b12
Filippo Miatto <miatto@gmail.com>
2024-11-16 12:19:22 -0800

    renamed files

--- js/button.js
+++ js/button.js
@@ -1,32 +1,34 @@
 // This script adds click event listeners to the back, previous, and next buttons
 
 const posts = [
-  "posts/post1.html",
-  "posts/post2.html",
-  "posts/post3.html",
-  "posts/post5.html",
-  "posts/post6.html",
-  "posts/post7.html",
-  "posts/post8.html",
-  "posts/post9.html"
+  "post1.html",
+  "post2.html",
+  "post3.html",
+  "post5.html",
+  "post6.html",
+  "post7.html",
+  "post8.html",
+  "post9.html"
 ];
 
-// Get the current post index
-const currentPost = window.location.pathname.split("/").pop();
-const currentIndex = posts.indexOf(currentPost);
+// Determine the current index based on the current page URL
+const currentPage = window.location.pathname.split('/').pop();
+let currentIndex = posts.indexOf(currentPage);
 
-document.getElementById("back-button").addEventListener("click", function() {
-  window.history.back();
+document.getElementById('back-button').addEventListener('click', () => {
+  window.location.href = '../index.html';
 });
 
-document.getElementById("prev-button").addEventListener("click", function() {
+document.getElementById('prev-button').addEventListener('click', () => {
   if (currentIndex > 0) {
-    window.location.href = posts[currentIndex - 1];
+    currentIndex--;
+    window.location.href = `../posts/${posts[currentIndex]}`;
   }
 });
 
-document.getElementById("next-button").addEventListener("click", function() {
+document.getElementById('next-button').addEventListener('click', () => {
   if (currentIndex < posts.length - 1) {
-    window.location.href = posts[currentIndex + 1];
+    currentIndex++;
+    window.location.href = `../posts/${posts[currentIndex]}`;
   }
 });
